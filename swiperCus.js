9a825b12fe3180449c7dee0502411d35eff2119d
RodoMedical <98857122+RodoMedical@users.noreply.github.com>
2022-02-11 10:17:33 -0800

    Add files via upload

--- swiperCus.js
+++ swiperCus.js
@@ -6,13 +6,183 @@
 //      - Video doesn't resize smoothly. Possibly because of wrong comparison
 
 
-const swiper = new Swiper('.swiper', {
+var hidden = document.getElementById("promo-video");
+var el;
+
+  /*IF play-button is pressed, load video*/
+
+
+  var bg = document.getElementById("bgvideo");
+  var bg2 = document.getElementById("bgvideo2");
+  var bg3 = document.getElementById("bgvideo3");
+  /*IF mfp-close is pressed, pause video*/
+  function pauseVid() {
+     bg.pause();
+  }
+  function playVid() {
+      bg.play();
+  }
+  
+  function pauseVid2() {
+    bg2.pause();
+ }
+ function playVid2() {
+     bg2.play();
+ }
+
+ function pauseVid3() {
+  bg3.pause();
+}
+function playVid3() {
+   bg3.play();
+}
+  function loadVid() {
+    document.querySelector(".pop-vid").load();
+}
+
+window.addEventListener("load", function(){
+  bg.play();
+  document.querySelector(".swiper-main").style.cursor = "default";
+});
+
+
+
+/*!
+* Determine if an element is in the viewport
+* (c) 2017 Chris Ferdinandi, MIT License, https://gomakethings.com
+* @param  {Node}    elem The element
+* @return {Boolean}      Returns true if element is in the viewport
+*/
+
+var isInViewport = function (elem) {
+var distance = elem.getBoundingClientRect();
+
+return (
+  distance.top >= 0 &&
+  distance.left >= 0 &&
+  distance.bottom <= (window.innerHeight + 330 || document.documentElement.clientHeight + 330) 
+  //distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+);
+};
+
+var mute_button = document.querySelector("#mute");
+var unmute_button = document.querySelector("#unmute");
+
+mute_button.addEventListener('click', function() {
+    if(bg3.muted) {  
+      bg3.muted = false;
+      mute_button.style.display = "none";
+      unmute_button.style.display = "block";
+    }
+});
+unmute_button.addEventListener('click', function() {
+      bg3.muted = true;
+      mute_button.style.display = "block";
+      unmute_button.style.display = "none";
+});
+
+var page_1 = document.querySelector('#page1');
+var page_2 = document.querySelector('#page2');
+var page_3 = document.querySelector('#page3');
+var page_4 = document.querySelector('#page4');
+var page_5 = document.querySelector('#page5');
+var page_6 = document.querySelector('#page6');
+var page_7 = document.querySelector('#page7');
+var page_8 = document.querySelector('#page8');
+var page_9 = document.querySelector('#page9');
+
+var mfp_container;
+
+
+
+
+document.addEventListener('click', function() {
+    mfp_container = document.querySelector(".pop-vid"); 
+
+  document.addEventListener('click', function(event) {
+    var isClickInsideElement = mfp_container.contains(event.target);
+    if (!isClickInsideElement) {
+      mfp_container.pause();
+      console.log("Outside");
+    } else {
+      console.log("inside");
+    }
+  });
+
+});
+
+
+window.addEventListener('load', function () {
+document.addEventListener('click', function() {
+  if(isInViewport(page_3)) {
+    playVid3();
+  } else {
+    pauseVid3();
+  }
+}, false);
+
+swiper.on('resize', function() {
+    if (swiper.height > bg.style.height) {
+        bg.style.height = swiper.height;
+        bg.style.width = "auto";
+    } else {
+        bg.style.width = "100%";
+        bg.style.height = "auto";  
+    }
+});
+
+
+swiper.on('transitionEnd', function() {
+            if(isInViewport(page_1)) {
+              playVid();
+            } else {
+              pauseVid();
+            }
+}, false); 
+
+swiper.on('transitionEnd', function() {
+  if(isInViewport(page_2)) {
+    playVid2();
+  } else {
+    pauseVid2();
+  }
+}, false); 
+
+swiper.on('transitionEnd', function() {
+  if(isInViewport(page_3)) {
+    playVid3();
+  } else {
+    pauseVid3();
+  }
+}, false); 
+
+swiper.on('transitionEnd', function() {
+  if(isInViewport(page_4)) {
+    var q = document.querySelector(".quote-text");
+    $(q).fadeIn();
+    /* document.querySelector(".quote").style.width = "100%";
+    document.querySelector(".quote-text").fadeIn(); */
+  } 
+}, false);
+
+});
+
+
+const swiper = new Swiper('.swiper-main', {
     // Optional parameters
+    //cssMode: true,
     slidesPerView: 1,
     direction: 'vertical',
     loop: false,
-    centeredSlides: true,  
-    speed: 500,
+     
+    speed: 1300,
+    shortSwipes: false,
+
+    forceToAxis: true,
+    preventClicks: true,
+    preventClicksPropagation: true,
+    preventInteractionOnTransition: true,
+
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
@@ -29,21 +199,17 @@ const swiper = new Swiper('.swiper', {
       onlyInViewport: false,
       pageUpDown: true,
     },
-    
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
       
     //Mousewheel
     mousewheel: {
-        
+        sensitivity: 0.01,
     },
     
     // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
+      draggable: true,
+      snapOnRelease: true
     },
     
     breakpoints: {
@@ -54,7 +220,8 @@ const swiper = new Swiper('.swiper', {
       },
   
       992: {
-        noSwiping: false
+        noSwiping: false,
+        grabCursor: true
       },
   
       1024: {
@@ -67,202 +234,32 @@ const swiper = new Swiper('.swiper', {
 
   });
 
-  var hidden = document.getElementById("promo-video");
-  var el;
+const carousel = new Swiper('.s-carousel', {
+  slidesPerView: 5,
+  slidesPerGroup: 5,
+  disabledOnInteraction: false,
+  pauseOnMouseEnter: true,
 
-    /*IF play-button is pressed, load video*/
- 
+  loop: true,
+  spaceBetween: 20,
+  direction: 'horizontal',
+  maxBackfaceHiddenSlides: 20,
 
-    var bg = document.getElementById("bgvideo");
-    var bg2 = document.getElementById("bgvideo2");
-    var bg3 = document.getElementById("bgvideo3");
-    /*IF mfp-close is pressed, pause video*/
-    function pauseVid() {
-       bg.pause();
-       console.log("Pause 1");
-    }
-    function playVid() {
-        bg.play();
-        console.log("Play 1");
-    }
-    
-    function pauseVid2() {
-      bg2.pause();
-      console.log("Pause 2");
-   }
-   function playVid2() {
-       bg2.play();
-       console.log("Play 2");
-   }
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-   function pauseVid3() {
-    bg3.pause();
-    console.log("Pause 3");
- }
- function playVid3() {
-     bg3.play();
-     console.log("Play 3");
- }
-    function loadVid() {
-      document.querySelector(".pop-vid").load();
-  }
-
-/*!
- * Determine if an element is in the viewport
- * (c) 2017 Chris Ferdinandi, MIT License, https://gomakethings.com
- * @param  {Node}    elem The element
- * @return {Boolean}      Returns true if element is in the viewport
- */
-
-var isInViewport = function (elem) {
-	var distance = elem.getBoundingClientRect();
-  console.log(window.innerHeight);
-  console.log(document.documentElement.clientHeight);
-  console.log(distance.top);
-  console.log(distance.left);
-
-	return (
-		distance.top >= 0 &&
-		distance.left >= 0 &&
-    distance.bottom <= (window.innerHeight + 330 || document.documentElement.clientHeight + 330) 
-		//distance.right <= (window.innerWidth || document.documentElement.clientWidth)
-	);
-};
-
-var page_1 = document.querySelector('#page1');
-var page_2 = document.querySelector('#page2');
-var page_3 = document.querySelector('#page3');
-var page_4 = document.querySelector('#page4');
-var page_5 = document.querySelector('#page5');
-var page_6 = document.querySelector('#page6');
-var page_7 = document.querySelector('#page7');
-var page_8 = document.querySelector('#page8');
-var page_9 = document.querySelector('#page9');
-
-var mfp_container;
-
-document.addEventListener('click', function() {mfp_container = document.querySelector(".mfp-content"); 
-  mfp_container.addEventListener('click', function(){
-        document.querySelector(".pop-vid").pause();
-    });
-  });
-
-
-
-
-
-
-
-window.addEventListener('load', function () {
-  document.addEventListener('click', function() {
-    if(isInViewport(page_3)) {
-      playVid3();
-    } else {
-      pauseVid3();
-    }
-  }, false);
-
-  swiper.on('resize', function() {
-      if (swiper.height > bg.style.height) {
-          bg.style.height = swiper.height;
-          bg.style.width = "auto";
-      } else {
-          bg.style.width = "100%";
-          bg.style.height = "auto";  
-      }
-  });
-
-
-  swiper.on('transitionEnd', function() {
-              if(isInViewport(page_1)) {
-                playVid();
-              } else {
-                pauseVid();
-              }
-  }, false); 
-
-  swiper.on('transitionEnd', function() {
-    if(isInViewport(page_2)) {
-      playVid2();
-    } else {
-      pauseVid2();
-    }
-  }, false); 
-
-  swiper.on('transitionEnd', function() {
-    if(isInViewport(page_3)) {
-      playVid3();
-    } else {
-      pauseVid3();
-    }
-  }, false); 
-
-  swiper.on('transitionEnd', function() {
-    if(isInViewport(page_4)) {
-      document.querySelector(".quote").style.width = "100%";
-      console.log("animated");
-    } 
-  }, false);
+  autoplay: {
+    delay: 5000,
+  },
 
 });
 
-/*
-swiper.on('transitionEnd', function() {
-  if(isInViewport(page_4)) {
-    playVid();
-    console.log("Playing Video");
-  } else {
-    pauseVid();
-    console.log("Pausing Video");
-  }
-}, false); 
 
-swiper.on('transitionEnd', function() {
-  if(isInViewport(page_5)) {
-    playVid();
-    console.log("Playing Video");
-  } else {
-    pauseVid();
-    console.log("Pausing Video");
-  }
-}, false); 
-
-swiper.on('transitionEnd', function() {
-  if(isInViewport(page_6)) {
-    playVid();
-    console.log("Playing Video");
-  } else {
-    pauseVid();
-    console.log("Pausing Video");
-  }
-}, false); 
-
-swiper.on('transitionEnd', function() {
-  if(isInViewport(page_7)) {
-    playVid();
-    console.log("Playing Video");
-  } else {
-    pauseVid();
-    console.log("Pausing Video");
-  }
-}, false); 
-
-swiper.on('transitionEnd', function() {
-  if(isInViewport(page_8)) {
-    playVid();
-    console.log("Playing Video");
-  } else {
-    pauseVid();
-    console.log("Pausing Video");
-  }
-}, false); 
-
-swiper.on('transitionEnd', function() {
-  if(isInViewport(page_9)) {
-    playVid();
-    console.log("Playing Video");
-  } else {
-    pauseVid();
-    console.log("Pausing Video");
-  }
-}, false); */
+$('#nav-logo').click(swiper,function(){
+    swiper.slideTo(0);
+})
+$('.linkTech').click(swiper, function() {
+    swiper.slideTo(5);
+})
